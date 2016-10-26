16483c00920ab284444cbe9fb32715161ec004a5
Amélie Cordier <amelie.cordier@univ-lyon1.fr>
2016-10-26 21:16:56 +0200

    Fix du template bootstrap customisable

--- documents/source/conf.py
+++ documents/source/conf.py
@@ -33,8 +33,7 @@ templates_path = ['_templates']
 # The suffix of source filenames.
 source_suffix = '.rst'
 
-# The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# The encoding of source files.#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -92,9 +91,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sphinx-ac-theme'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx-ac-theme'
+
+#varBootStrap = sphinx_bootstrap_theme.get_html_theme_path().append('../themes')
+#varMe = '../themes'
+
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()  + ['../themes']
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
